d5a8effbbba3e163e5f924179357e7f66dd09aa6
Jorge Echeverria <jfloresjef365@gmail.com>
2025-07-10 15:53:24 -0600

    Update controlador_reportes.py
    Cambios

--- controlador_reportes.py
+++ controlador_reportes.py
@@ -1,67 +1,46 @@
 from modelo_reportes import ReportesModel
-from vista_reportes import ReportesView
 
 class ReportesController:
-    def __init__(self):
+    def __init__(self, view):
         self.model = ReportesModel()
-        self.view = ReportesView()
-        self.connect_signals()
-        self.load_initial_data()
+        self.view = view
+        self.view.btn_exportar.clicked.connect(self.exportar_a_excel)
+        self.view.btn_actualizar.clicked.connect(self.actualizar_datos)
+        self.mostrar_reporte("Semanal")  # Valor por defecto
 
-    def connect_signals(self):
-        self.view.buscar_btn.clicked.connect(self.update_reports)
-        self.view.limpiar_btn.clicked.connect(self.clear_filters)
-        self.view.exportar_btn.clicked.connect(self.export_to_excel)
-        self.view.actualizar_btn.clicked.connect(self.update_reports)
+    def mostrar_reporte(self, periodo):
+        self.view.set_periodo(periodo)
+        end_date = datetime.now().strftime("%Y-%m-%d")
+        start_date = self.model.adjust_date_range(end_date, periodo)
 
-    def load_initial_data(self):
-        clientes = self.model.get_clientes()
-        productos = self.model.get_productos()
-        self.view.populate_clientes(clientes)
-        self.view.populate_productos(productos)
-        self.update_reports()
+        # Datos de ventas por fecha
+        ventas = self.model.get_ventas_por_fecha(start_date, end_date)
+        fechas = [v[0] for v in ventas]
+        totales = [v[1] for v in ventas]
+        self.view.actualizar_grafico_ventas(fechas, totales)
 
-    def update_reports(self):
-        filters = self.view.get_filter_values()
-        start_date = filters['start_date']
-        end_date = filters['end_date']
-        cliente_id = filters['cliente_id'] if filters['cliente_id'] else None
-        producto_id = filters['producto_id'] if filters['producto_id'] else None
-        period = filters['period']
-
-        # Adjust date range for weekly reports
-        if period == "Semanal":
-            start_date = self.model.adjust_date_range(end_date, period)
-            self.view.set_start_date(start_date)
-
-        # Get data from model
-        total_ventas, total_pedidos = self.model.get_total_ventas(start_date, end_date, cliente_id)
-        top_cliente = self.model.get_top_cliente(start_date, end_date)
-        productos_vendidos = self.model.get_productos_vendidos(start_date, end_date, producto_id)
-        detalle_ventas = self.model.get_detalle_ventas(start_date, end_date)
-        ventas_por_fecha = self.model.get_ventas_por_fecha(start_date, end_date)
+        # Top clientes
         top_clientes = self.model.get_top_clientes(start_date, end_date)
+        clientes = [c[0] for c in top_clientes]
+        totales_clientes = [c[1] for c in top_clientes]
+        self.view.actualizar_grafico_top_clientes(clientes, totales_clientes)
 
-        # Prepare data for graphs
-        best_products = productos_vendidos[-5:] if productos_vendidos else []
-        worst_products = productos_vendidos[:5] if productos_vendidos else []
-        top_producto = productos_vendidos[-1][0] if productos_vendidos else '-'
+        # Productos más y menos vendidos
+        productos_vendidos = self.model.get_productos_vendidos(start_date, end_date)
+        productos = [p[0] for p in productos_vendidos]
+        cantidades = [p[1] for p in productos_vendidos]
+        self.view.actualizar_grafico_productos(productos, cantidades)
 
-        # Update view
-        self.view.update_summary(total_ventas, total_pedidos, top_producto, top_cliente)
-        self.view.update_table(detalle_ventas)
-        self.view.update_graphs(ventas_por_fecha, top_clientes, best_products, worst_products)
+        # Detalle de ventas para la tabla
+        detalle_ventas = self.model.get_detalle_ventas(start_date, end_date)
+        self.view.actualizar_tabla(detalle_ventas)
 
-    def clear_filters(self):
-        self.view.reset_filters()
-        self.update_reports()
+    def exportar_a_excel(self):
+        end_date = datetime.now().strftime("%Y-%m-%d")
+        start_date = self.model.adjust_date_range(end_date, self.view.periodo)
+        self.model.export_to_excel(start_date, end_date)
 
-    def export_to_excel(self):
-        filters = self.view.get_filter_values()
-        self.model.export_to_excel(filters['start_date'], filters['end_date'])
-
-    def show(self):
-        self.view.show()
-
-    def close(self):
+    def actualizar_datos(self):
         self.model.close()
+        self.model = ReportesModel()
+        self.mostrar_reporte(self.view.periodo)
