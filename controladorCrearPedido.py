d988d76337404c128019d78dd261fb809da4cc9d
Annie905 <reinasosa2024@gmail.com>
2025-07-13 16:49:31 -0600

    Update controladorCrearPedido.py

--- controladorCrearPedido.py
+++ controladorCrearPedido.py
@@ -1,5 +1,5 @@
 from PyQt6.QtWidgets import QMessageBox, QInputDialog
-from Modelo.modeloDato import Modelo
+from Distribuidora_pedidos.Modelo.modeloPedido import Modelo
 from Vista.crearPedido import CrearPedidos
 
 class ControladorCrearPedidos:
