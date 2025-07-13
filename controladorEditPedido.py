88ac2ad738bb41edcc8fcf5ce0b20c78d43e821e
Annie905 <reinasosa2024@gmail.com>
2025-07-13 16:52:22 -0600

    Update controladorEditPedido.py

--- controladorEditPedido.py
+++ controladorEditPedido.py
@@ -1,5 +1,5 @@
 from PyQt6.QtWidgets import QMessageBox
-from Distribuidora_pedidos.Modelo.modeloPedido import Modelo
+from Modelo.modeloPedido import Modelo
 from Vista.editarPedido import EditarPedido
 from Controlador.controladorCrearPedido import ControladorCrearPedidos
 
