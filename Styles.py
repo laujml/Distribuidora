d30151df05d42ec36d7725033f3ef8c6ec6e0645
Annie905 <reinasosa2024@gmail.com>
2025-07-14 13:49:03 -0600

    Update Styles.py primera version

--- Styles.py
+++ Styles.py
@@ -1,136 +1,153 @@
 class Styles:
-    """Clase para gestionar estilos de la interfaz de usuario de la aplicación de distribuidora."""
-    
-    # Paleta de colores más sobria
-    BACKGROUND = "#4d5a62"  # Fondo más claro y sobrio
-    BUTTON_BG = "#969a9a"   # Gris claro para botones
-    BUTTON_HOVER = "#676d71"  # Gris más oscuro para hover
-    BORDER = "#676d71"      # Color de bordes
-    TEXT = "#969fa3"        # Color de texto
-    LABEL_TEXT = "#969a9a"  # Color de texto para etiquetas
+    """Estilos generales reutilizables para la aplicación."""
+
+    PRIMARY_TEXT = "#4d5a62"  # Azul sobrio
+    BACKGROUND = "#4d5a62"    # Fondo gris oscuro general
+    WHITE = "#ffffff"
+    BORDER = "#dee2e6"
+    HIGHLIGHT = "#f5f7fa"
 
     @staticmethod
-    def get_app_stylesheet():
-        """Devuelve la hoja de estilos para toda la aplicación."""
+    def get_stylesheet():
         return f"""
+            /* Fondo global */
             QWidget {{
                 background-color: {Styles.BACKGROUND};
-                color: {Styles.TEXT};
-                font-family: Arial, sans-serif;
-                font-size: 14px;
+                color: #f0f0f0;
+                font-family: 'Poppins', sans-serif;
+                font-size: 12px;
             }}
-        """
 
-    @staticmethod
-    def get_button_stylesheet():
-        """Devuelve la hoja de estilos para widgets QPushButton."""
-        return f"""
-            QPushButton {{
-                background-color: {Styles.BUTTON_BG};
-                color: {Styles.TEXT};
-                border: 1px solid {Styles.BORDER};
-                padding: 8px;
-                border-radius: 3px;
-            }}
-            QPushButton:hover {{
-                background-color: {Styles.BUTTON_HOVER};
-            }}
-            QPushButton:pressed {{
-                background-color: {Styles.BACKGROUND};
-            }}
-        """
-
-    @staticmethod
-    def get_combo_box_stylesheet():
-        """Devuelve la hoja de estilos para widgets QComboBox."""
-        return f"""
-            QComboBox {{
-                background-color: {Styles.BACKGROUND};
-                color: {Styles.TEXT};
-                border: 1px solid {Styles.BORDER};
-                padding: 5px;
-            }}
-            QComboBox:hover {{
-                background-color: {Styles.BUTTON_HOVER};
-            }}
-            QComboBox::drop-down {{
-                border: none;
-            }}
-            QComboBox::down-arrow {{
-                image: none;
-                width: 10px;
-                height: 10px;
-            }}
-        """
-
-    @staticmethod
-    def get_date_edit_stylesheet():
-        """Devuelve la hoja de estilos para widgets QDateEdit."""
-        return f"""
-            QDateEdit {{
-                background-color: {Styles.BACKGROUND};
-                color: {Styles.TEXT};
-                border: 1px solid {Styles.BORDER};
-                padding: 5px;
-            }}
-            QDateEdit:hover {{
-                background-color: {Styles.BUTTON_HOVER};
-            }}
-            QDateEdit::drop-down {{
-                border: none;
-            }}
-            QDateEdit::down-button, QDateEdit::up-button {{
-                background-color: {Styles.BUTTON_BG};
-            }}
-        """
-
-    @staticmethod
-    def get_table_widget_stylesheet():
-        """Devuelve la hoja de estilos para widgets QTableWidget."""
-        return f"""
-            QTableWidget {{
-                background-color: {Styles.BACKGROUND};
-                color: {Styles.TEXT};
-                border: 1px solid {Styles.BORDER};
-                gridline-color: {Styles.BORDER};
-            }}
-            QTableWidget::item {{
-                border: none;
-            }}
-            QTableWidget::item:selected {{
-                background-color: {Styles.BUTTON_HOVER};
-            }}
-            QHeaderView::section {{
-                background-color: {Styles.BUTTON_BG};
-                color: {Styles.TEXT};
-                border: 1px solid {Styles.BORDER};
-                padding: 4px;
-            }}
-        """
-
-    @staticmethod
-    def get_label_stylesheet():
-        """Devuelve la hoja de estilos para widgets QLabel."""
-        return f"""
             QLabel {{
-                color: {Styles.LABEL_TEXT};
+                color: #f0f0f0;
                 background-color: transparent;
             }}
-        """
 
-    @staticmethod
-    def apply_styles(widget):
-        """Aplica la hoja de estilos adecuada según el tipo de widget."""
-        from PyQt6.QtWidgets import QPushButton, QComboBox, QDateEdit, QTableWidget, QLabel
-        if isinstance(widget, QPushButton):
-            widget.setStyleSheet(Styles.get_button_stylesheet())
-        elif isinstance(widget, QComboBox):
-            widget.setStyleSheet(Styles.get_combo_box_stylesheet())
-        elif isinstance(widget, QDateEdit):
-            widget.setStyleSheet(Styles.get_date_edit_stylesheet())
-        elif isinstance(widget, QTableWidget):
-            widget.setStyleSheet(Styles.get_table_widget_stylesheet())
-        elif isinstance(widget, QLabel):
-            widget.setStyleSheet(Styles.get_label_stylesheet())
-        else:
-            widget.setStyleSheet(Styles.get_app_stylesheet())
+            QLineEdit, QComboBox, QSpinBox, QDoubleSpinBox,QDateEdit {{
+                background-color: {Styles.WHITE};
+                color: {Styles.PRIMARY_TEXT};
+                border: 1px solid {Styles.BORDER};
+                padding: 4px 8px;
+                border-radius: 6px;
+            }}
+
+            QTableWidget {{
+                background-color: {Styles.WHITE};
+                color: {Styles.PRIMARY_TEXT};
+                border: 1px solid {Styles.BORDER};
+                gridline-color: {Styles.BORDER};
+                font-size: 12px;
+            }}
+
+            QTableWidget::item:selected {{
+                background-color: {Styles.HIGHLIGHT};
+                color: {Styles.PRIMARY_TEXT};
+            }}
+
+            QHeaderView::section {{
+                background-color: {Styles.HIGHLIGHT};
+                color: {Styles.PRIMARY_TEXT};
+                border: 1px solid {Styles.BORDER};
+                padding: 4px;
+                font-weight: bold;
+            }}
+
+            QPushButton {{
+                background: #ffffff;
+                color: {Styles.PRIMARY_TEXT};
+                border-radius: 6px;
+                padding: 4px 12px;
+                font-size: 11px;
+                border: 1px solid {Styles.PRIMARY_TEXT};
+                min-height: 26px;
+                font-weight: bold;
+            }}
+
+            QPushButton:hover {{
+                background-color: #f0f0f0;
+            }}
+
+            QPushButton:pressed {{
+                background-color: #e0e0e0;
+            }}
+
+            /* Solo el panel lateral izquierdo */
+            QWidget#panelIzquierdo {{
+                background-color: {Styles.WHITE};
+                color: {Styles.PRIMARY_TEXT};
+                border-right: 1px solid {Styles.BORDER};
+            }}
+
+            /* Estilo para el número de pedido */
+            QLabel#pedidoNumero {{
+                background-color: {Styles.WHITE};
+                color: {Styles.PRIMARY_TEXT};
+                font-size: 14px;
+                font-weight: bold;
+                padding: 6px 8px;
+                border-radius: 4px;
+            }}
+
+            QListWidget#panelIzquierdo {{
+            background-color: white;
+            color: #4d5a62;
+            border: 1px solid #dee2e6;
+            }}
+
+            QListWidget#menuLateral {{
+            background-color: white;
+            color: #4d5a62;  /* El azul oscuro que usas en el resto */
+            border: none;
+            font-weight: bold;
+            }}
+
+            QListWidget#menuLateral::item {{
+                padding: 6px 12px;
+            }}
+
+            QListWidget#menuLateral::item:selected {{
+                background-color: #d0e1ff;  /* un azul clarito para el item seleccionado */
+                color: #1a2b4c;  /* un azul más oscuro para el texto seleccionado */
+            }}
+
+            QWidget#contenedorTabla {{
+            background-color: white;
+            }}
+
+            /* Fondo del desplegable (la lista que se abre) */
+            QComboBox QAbstractItemView {{
+                background-color: white;
+                color: {Styles.PRIMARY_TEXT};
+                border: 1px solid {Styles.BORDER};
+                selection-background-color: {Styles.HIGHLIGHT};
+                selection-color: {Styles.PRIMARY_TEXT};
+            }}
+
+            QSpinBox::up-button, QDoubleSpinBox::up-button {{
+            subcontrol-origin: border;
+            subcontrol-position: top right;
+            width: 16px;
+            border-left: 1px solid {Styles.BORDER};
+            border-bottom: 1px solid {Styles.BORDER};
+            background-color: {Styles.WHITE};
+            }}
+
+            QSpinBox::down-button, QDoubleSpinBox::down-button {{
+                subcontrol-origin: border;
+                subcontrol-position: bottom right;
+                width: 16px;
+                border-left: 1px solid {Styles.BORDER};
+                border-top: 1px solid {Styles.BORDER};
+                background-color: {Styles.WHITE};
+            }}
+
+            /* Cambiar color de fondo de botones al hacer hover */
+            QSpinBox::up-button:hover, QDoubleSpinBox::up-button:hover {{
+                background-color: #e6f0ff;
+            }}
+
+            QSpinBox::down-button:hover, QDoubleSpinBox::down-button:hover {{
+                background-color: #e6f0ff;
+            }}
+        """
+    
