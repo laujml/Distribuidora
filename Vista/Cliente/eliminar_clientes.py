19b94c248d9d028635801aee3489d9d704f44496
Annie905 <reinasosa2024@gmail.com>
2025-07-18 17:11:49 -0600

    Update eliminar_clientes.py

--- Vista/Cliente/eliminar_clientes.py
+++ Vista/Cliente/eliminar_clientes.py
@@ -3,7 +3,7 @@ from PyQt6.QtWidgets import (
 )
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QFont
-from styles.styles import Styles
+from Recursos.Styles import Styles
 
 class EliminarClientes(QWidget):
     def __init__(self, controller, regresar_callback=None):
