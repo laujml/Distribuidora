40c9675994f1c291abeac4c41fc32b151f43ad34
Laura Angel <laura.an.la03@gmail.com>
2025-07-18 19:35:37 -0600

    Update dialogo_crear_usuario.py
    comentarios

--- Vista/Usuario/dialogo_crear_usuario.py
+++ Vista/Usuario/dialogo_crear_usuario.py
@@ -1,4 +1,7 @@
-# vista/dialogo_crear_usuario.py
+# Se hace uso de PyQt6 y Controlador.usuario
+#Dialogo para crear usuario es una ventana emergente que requiere de los datos "rol", "usuario" y "contraseña".
+# Se ejecuta unicamente al entrar por medio de la pantalla del administrador haciendo click en "Crear nuevo usuario"
+#Los datos quedan almacenados en la Base de Datos de la distribuidora
 from PyQt6.QtWidgets import QDialog, QVBoxLayout, QLabel, QLineEdit, QPushButton, QComboBox, QMessageBox
 from Controlador.Usuario.usuario_controlador import registrar_nuevo_usuario
 
@@ -16,6 +19,7 @@ class CrearUsuarioDialog(QDialog):
         self.password.setPlaceholderText("Contraseña")
         self.password.setEchoMode(QLineEdit.EchoMode.Password)
 
+        #Conexion del boton guardar con guardar_usuario
         btn_guardar = QPushButton("Guardar")
         btn_guardar.clicked.connect(self.guardar_usuario)
 
@@ -26,6 +30,8 @@ class CrearUsuarioDialog(QDialog):
         layout.addWidget(btn_guardar)
         self.setLayout(layout)
 
+    #Se pide la informacion de usuario, password y el rol de usuario o administrador. Si no se llenan todos los campos se muestra una advertencia y no se guardan
+    # hasta que los campos sean completados. Se muestra confirmacion de guardado
     def guardar_usuario(self):
         nombre = self.usuario.text()
         clave = self.password.text()
