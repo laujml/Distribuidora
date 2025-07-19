7823d6bc7b351b4f548e8569c09e0ab4edb037ce
Laura Angel <laura.an.la03@gmail.com>
2025-07-18 19:53:10 -0600

    Update pantalla_bienvenida.py
    comentarios

--- Vista/Usuario/pantalla_bienvenida.py
+++ Vista/Usuario/pantalla_bienvenida.py
@@ -1,3 +1,6 @@
+# Uso de PQt6, os(para lectura) y sys
+# La pantalla de bienvenida es la primera pantalla a mostrarse al correr la aplicacion. Su fin es meramente estetico para poder trasladarse al login.
+# Ventana no indispensable y no afecta al resto del codigo
 from PyQt6.QtWidgets import QWidget, QLabel, QPushButton, QVBoxLayout, QSizePolicy
 from PyQt6.QtGui import QPixmap, QFont, QPainter, QPainterPath
 from PyQt6.QtCore import Qt, QSize
@@ -14,11 +17,12 @@ class PantallaBienvenida(QWidget):
         layout = QVBoxLayout()
         layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
         
-        # Logo
+        # Carga de Logo
         logo_label = QLabel(self)
         logo_path = "Recursos/logo.jpg"
         pixmap = QPixmap(logo_path)
-        
+        #Ajuste deltamaño
+        #En caso de que el logo no cargue o no exista se proyecta la palabra "LOGO"
         if os.path.exists(logo_path):
             pixmap = QPixmap(logo_path).scaled(240, 240, Qt.AspectRatioMode.KeepAspectRatioByExpanding, Qt.TransformationMode.SmoothTransformation)
             rounded_pixmap = self.redondear_pixmap(pixmap, 240)
@@ -30,11 +34,11 @@ class PantallaBienvenida(QWidget):
         logo_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         logo_label.setSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
         
-        # Título
+        # Título de bienvenida
         label = QLabel("¡Bienvenido!", self)
         label.setObjectName("titulo")
         
-        # Botón
+        # Botón de iniciar sesion conectado al login
         boton = QPushButton("Iniciar sesión", self)
         boton.setFixedSize(250, 35)
         boton.clicked.connect(self.ir_a_login)
@@ -58,6 +62,6 @@ class PantallaBienvenida(QWidget):
         painter.drawPixmap(0, 0, pixmap)
         painter.end()
         return rounded
-    
+    #Cambio de index a 1(Pantalla de login)
     def ir_a_login(self):
         self.stack.setCurrentIndex(1)
