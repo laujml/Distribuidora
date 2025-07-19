ef65db5d76802dd75270279aecf710bab1960903
Laura Angel <laura.an.la03@gmail.com>
2025-07-18 20:10:49 -0600

    Update usuario_modelo.py
    comentarios

--- Modelo/usuario_modelo.py
+++ Modelo/usuario_modelo.py
@@ -1,6 +1,7 @@
-# modelo/usuario_modelo.py
+# Uso de Modelo.db_config
+# Modulo que contiene la validacion de credenciales, eliminacion, modificacion, agregar, obtener id y usuario
 from Modelo.db_config import conectar
-
+#Funcion para obtener todos los usuarios registrados usando sql
 def obtener_usuarios():
     conn = conectar()
     cursor = conn.cursor(dictionary=True)
@@ -14,13 +15,13 @@ def obtener_usuarios():
             "rol": u["tipoUsuario"]
         } for u in usuarios
     }
-
+#Funcion para obtener el siguiente ID disponible que se le puede brindar
 def obtener_nuevo_id(conn):
     cursor = conn.cursor()
     cursor.execute("SELECT MAX(ID_Usuario) FROM Usuario")
     max_id = cursor.fetchone()[0]
     return (max_id or 0) + 1
-
+#Funcion para agregar un nuevo usuario a la base de datos
 def agregar_usuario(usuario, clave, rol):
     conn = conectar()
     cursor = conn.cursor()
@@ -31,7 +32,7 @@ def agregar_usuario(usuario, clave, rol):
     )
     conn.commit()
     conn.close()
-
+#Funcion que modifica la password de un usuario 
 def modificar_usuario(usuario, nueva_clave):
     conn = conectar()
     cursor = conn.cursor()
@@ -41,14 +42,14 @@ def modificar_usuario(usuario, nueva_clave):
     )
     conn.commit()
     conn.close()
-
+#Funcion que elimina un usuario
 def eliminar_usuario(usuario):
     conn = conectar()
     cursor = conn.cursor()
     cursor.execute("DELETE FROM Usuario WHERE nombreUsuario=%s", (usuario,))
     conn.commit()
     conn.close()
-
+#Funcion que valida si el usuario y la password coinciden con los registrados
 def validar_credenciales(usuario, clave):
     conn = conectar()
     cursor = conn.cursor(dictionary=True)
